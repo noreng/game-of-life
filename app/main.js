f49dcfbba0bebbaa4f4c09944bb6866fd1ef34b4
Norbert Pozsonyi <norbert.pozsonyi@timelinepi.com>
2017-11-25 11:05:31 +0100

    create Game, store living cells, get neighbours position
    - TODO count living cells around a position

---
+++ app/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = class Game {
+    constructor() {
+        this.alives = new Set();
+    }
+
+    addCell(x,y) {
+        this.alives.add('foo');
+    }
+
+    getPositionKey(row, col) {
+        return `${row},${col}`;
+    }
+
+    getNeighboursCoords(row,col) {
+        const coords = [];
+        const topLeftRow = row - 1;
+        const topLeftCol = col - 1;
+        const indexesFromTopLeft = [
+            [0,1,2],
+            [0,  2],
+            [0,1,2],
+        ];
+        indexesFromTopLeft.forEach((columns, rowIdx) => {
+            columns.forEach(colIdx => {
+                const key = this.getPositionKey(topLeftRow + rowIdx, topLeftCol + colIdx);
+                coords.push(key);
+            });
+        });
+        return coords;
+    }
+};
